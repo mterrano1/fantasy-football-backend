2ecc7b77055a38a7a6212ecfae00ffcb9bb8bacd
Christopher Terrano <mterrano1@yahoo.com>
2022-05-17 16:31:42 -0500

    include players in get requests and order fantasy teams by points

--- app/controllers/fantasy_teams_controller.rb
+++ app/controllers/fantasy_teams_controller.rb
@@ -1,14 +1,13 @@
 class FantasyTeamsController < ApplicationController
-    set :default_content_type, 'application/json'
 
     get '/fantasy-teams' do
-        fantasy_teams = FantasyTeam.all
-        fantasy_teams.to_json
+        fantasy_teams = FantasyTeam.all.order(:points)
+        fantasy_teams.to_json(include: :players)
       end
 
     get '/fantasy-teams/:id' do
         fantasy_team = FantasyTeam.find_by(id: params[:id])
-        fantasy_team.to_json
+        fantasy_team.to_json(include: :players)
     end
 
   end
